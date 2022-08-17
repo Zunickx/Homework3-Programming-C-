dfbe32f3fb27034c324a9093e943b5d977cf929f
Zunickx <Zunickx@gmail.com>
2022-08-17 21:11:46 +0300

    Task001 Задача на проверку числа. Является ли оно палиндромом

---
+++ Task001/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+Console.WriteLine("Введите пятизначное число:");
+string num = Console.ReadLine()!;
+string numReverse = num; // примитивный тип копируется не по ссылке, а по значению
+
+char[] reverseNumArr = numReverse.ToCharArray(); // преобразовали строку к массиву символов
+Array.Reverse(reverseNumArr);
+numReverse = new string(reverseNumArr);
+
+if(num == numReverse)
+{
+    Console.WriteLine("Это палиндром");
+}
+else
+{
+    Console.WriteLine("Это не палиндром");
+}
