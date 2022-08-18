2d9fb11411c69a462f01fc1bcd6e230caff73d67
Zunickx <Zunickx@gmail.com>
2022-08-18 21:40:42 +0300

    Task003 Задача на вывод таблицы кубов чисел от 1 до N

---
+++ Task003/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите целое число:");
+string num = Console.ReadLine()!;
+
+for (int i = 1; i <= int.Parse(num); i++)
+{
+    Console.WriteLine(i + "^3 = " + Math.Pow(i, 3));
+}
